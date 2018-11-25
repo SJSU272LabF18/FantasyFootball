434fbf5aac080277fc0b3545e7501bff37696ff1
Anthony Vo <avyu94@gmail.com>
2018-11-24 17:59:00 -0800

    added detail player view

--- analyze.py
+++ analyze.py
@@ -29,4 +29,24 @@ class Analyze:
 
 		return final_result
 
+	def get_player_details(self, player_id):
+		mycursor = self.mysql.connection.cursor()
+		sql = "SELECT player_id, pos, team, year, games_played, rush, rush_yards, rush_td, target, catch, catch_yards, catch_td, pass, complete, pass_yards, pass_td, interceptions, fumbles FROM player_year_stats WHERE player_id = %s"
+		#print(val)
+		mycursor.execute(sql, [int(player_id)])
+		myresult = mycursor.fetchall()
+		final_result = []
 
+		for result in myresult:
+			final_result.append(result)
+
+
+		mycursor = self.mysql.connection.cursor()
+		sql = "SELECT id,firstname,lastname FROM players"
+		#print(val)
+		mycursor.execute(sql)
+		myresult = mycursor.fetchone()
+		player_detail = myresult
+
+
+		return player_detail,final_result
--- main.py
+++ main.py
@@ -18,7 +18,10 @@ def playerPool():
 	players = a1.get_sorted_players()
 	return render_template('playerPool.html',data = players)
 
-
+@app.route("/detail/<player_id>")
+def detail(player_id):
+	player, player_record = a1.get_player_details(player_id)
+	return render_template('detail.html',data = {"player":player,"player_record":player_record})
 
 
 
